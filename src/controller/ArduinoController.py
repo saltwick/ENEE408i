e1e1b198429ade9872ee57ae26e39752af83f5f6
Sam Saltwick <sam@saltwick.com>
2019-09-20 20:15:26 -0400

    Turn signals

---
+++ src/controller/ArduinoController.py
@@ -0,0 +1,15 @@
+import serial
+
+class ArduinoController():
+    
+    def __init__(self, device, baud):
+        self.device = device
+        self.baud_rate = baud
+        self.ser = None
+    
+    def start(self):
+        self.ser = serial.Serial(self.device, self.baud_rate)
+
+    def send_message(self, msg):
+        #ser.write(str(msg))
+        print(msg.hex())
